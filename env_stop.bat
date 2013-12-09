092c5de3a8c6ebd0b264f3b29ca2605290e935f3
Jcan <jcan@eyebuydirect.com>
2013-12-10 00:26:03 +0800

    init

---
+++ env_stop.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd /D %~dp0
+
+echo Stopping Nginx ...
+cd nginx-1.5.7
+nginx.exe -s stop 2>nul
+taskkill /F /IM nginx.exe 2>nul
+cd ..
+
+echo Stopping PHP FastCGI ...
+taskkill /F /IM php-cgi.exe 2>nul
